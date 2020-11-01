6f3e941084d13a56ee2e918421f36d90c88f92c7
emlag <edl926@g.harvard.edu>
2020-11-01 19:43:55 +0800

    add lab3

--- Labs/app/src/main/java/edu/cis/labs_22/Lab3/TestHandoutMaster.java
+++
@@ -1,4 +0,0 @@
-package edu.cis.labs_22.Lab3;
-
-public class TestHandoutMaster {
-}
---
+++ Labs/app/src/test/java/edu/cis/labs_22/Lab3Tests.java
@@ -0,0 +1,14 @@
+package edu.cis.labs_22;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Lab3Tests {
+
+    @Test
+    public void addBookTest()
+    {
+
+    }
+}
